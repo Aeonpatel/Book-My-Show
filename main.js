705d33401b87c89d5db3e5205763394f59769cfa
Aeonpatel <85121945+Aeonpatel@users.noreply.github.com>
2022-02-02 18:11:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+tickets=document.getElementByI("tickets").value;
+
+function afternoon(){
+document.getElementById("show").innerHTML=tickets +"are booked for the afternoon show.!!!!!!!";
+}
+function morning(){
+document.getElementById("show").innerHTML=tickets +"are booked for the morning show.!!!!!!!";
+}
